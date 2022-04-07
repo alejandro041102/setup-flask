ba6568c156735b8dea4bdaa0617030fa1f020542
alejandro041102 <alejandro11lapira@gmail.com>
2022-04-07 12:08:50 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask (__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello World"
+    return "Pogi ako"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
